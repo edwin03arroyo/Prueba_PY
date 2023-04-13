7fc5d8afe97fca41a046c3cb2fb75067135ba7b9
GoldenPlox <edwin.arroyo@ucuenca.edu.ec>
2023-04-13 08:13:47 -0500

    Lista.py

---
+++ Lista.py
@@ -0,0 +1,3 @@
+#Crea una lista con 7 nombres de tus amigos y muestra por pantalla el número de elementos de la lista.
+amigos = ["Nilo","Aron","Edwin","Dayanna","Ariel"]
+print("Total de Amigos: ", len(amigos))
