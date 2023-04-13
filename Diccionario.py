b2037947cd4282df8cd303d8df14d63e22d219db
GoldenPlox <edwin.arroyo@ucuenca.edu.ec>
2023-04-13 08:05:09 -0500

    Diccionario.py

---
+++ Diccionario.py
@@ -0,0 +1,17 @@
+ventas = {"manzanas": 10, "naranjas": 5, "peras": 8}
+print(ventas["manzanas"])
+
+ventas = {"manzanas": 10, "naranjas": 5, "peras": 8}
+ventas["limones"] = 12
+ventas["sandías"] = 3
+ventas["melones"] = 5
+print(ventas)
+
+ventas = {"manzanas": 10, "naranjas": 5, "peras": 8, "limones": 12, "sandías": 3, "melones": 5}
+print(ventas.keys())
+
+ventas = {"manzanas": 10, "naranjas": 5, "peras": 8, "limones": 12, "sandías": 3, "melones": 5}
+print(list(ventas.keys())) 
+
+
+
